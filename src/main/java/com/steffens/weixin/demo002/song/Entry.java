b5d4198123bd68bab1376f9cdf4273f36464ca18
正义之剑 <yuhaibao324@126.com>
2018-12-16 03:29:10 +0800

    修正错误 引入类不正确

--- src/main/java/com/steffens/weixin/demo002/song/Entry.java
+++ src/main/java/com/steffens/weixin/demo002/song/Entry.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
  * Created by Song on 2017/2/15.
  * 项目启动入口，配置包根路径
  */
+
 //@SpringBootApplication
 @ComponentScan(basePackages = "com.song")
 public class Entry {
